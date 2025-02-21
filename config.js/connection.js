4f1c0c245f11c7dfeee624fecab17a63ca71d67b
sona mittal <sonamittal285@gmail.com>
2025-02-21 23:02:30 +0530

    O

--- config.js/connection.js
+++ config.js/connection.js
@@ -1,15 +1,21 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-const connection = mysql.createConnection({
+const pool =  mysql.createPool({
     host: process.env.DB_HOST,
     port:process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-connection.connect((err) => {
-    if (err) throw err;
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
     console.log('Connected to MySQL database');
+    connection.release(); 
 });
-
-module.exports = connection;
+module.exports =  pool;
